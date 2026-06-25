7118af546c1555347cfdc75726346d4992065548
Shanujan <shanushanujan234@gmail.com>
2026-06-25 23:36:14 +0530

    User model created

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,5 @@
 from app.models.student_model import Student
+from app.models.student_model import User
 
-__all__ = ["Student"]
+__all__ = ["Student", "User"]
 
---
+++ app/models/user_model.py
@@ -0,0 +1,11 @@
+from app.extensions import db
+from app.utils import utc_now
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    password = db.Column(db.String(255), nullable=False)
+    role = db.Column(db.String(20), nullable=False, default="student")
+    created_at = db.Column(db.DateTime, default=utc_now)
