25230fb23bede8245cc684d4d535e1f90542cab1
Shanujan <shanushanujan234@gmail.com>
2026-06-26 00:09:32 +0530

    __init__.py inside the model updated

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,10 +1,11 @@
 from app.models.student_model import Student
 from app.models.course_model import Course
+from app.models.user_model import User
 
 
 
 
-__all__ = ["Student","Course"]
+__all__ = ["Student","Course", "User"]
 
 
 
